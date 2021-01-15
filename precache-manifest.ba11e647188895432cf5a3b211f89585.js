04693a7d997fe0cab56b353a0a3b4ee13f060412
Vasily Mishanin <vasilymishanin@gmail.com>
2021-01-15 21:22:35 +0300

    Updates

---
+++ precache-manifest.ba11e647188895432cf5a3b211f89585.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22cb1468126a30c0e1a2a75866f0a20e",
+    "url": "/mosh-vidly/index.html"
+  },
+  {
+    "revision": "a9b5c1205658d2b8e16d",
+    "url": "/mosh-vidly/static/css/2.4bdbf95c.chunk.css"
+  },
+  {
+    "revision": "e158eb9ec4384b9d776a",
+    "url": "/mosh-vidly/static/css/main.5b36f601.chunk.css"
+  },
+  {
+    "revision": "a9b5c1205658d2b8e16d",
+    "url": "/mosh-vidly/static/js/2.469ba402.chunk.js"
+  },
+  {
+    "revision": "29f151c9adce85cb2f6cb6546d70845d",
+    "url": "/mosh-vidly/static/js/2.469ba402.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e158eb9ec4384b9d776a",
+    "url": "/mosh-vidly/static/js/main.eda0f16c.chunk.js"
+  },
+  {
+    "revision": "f5982e87bda38608341d",
+    "url": "/mosh-vidly/static/js/runtime-main.172b5e64.js"
+  },
+  {
+    "revision": "085b1dd8427dbeff10bd55410915a3f6",
+    "url": "/mosh-vidly/static/media/fa-brands-400.085b1dd8.ttf"
+  },
+  {
+    "revision": "0fabb6606be4c45acfeedd115d0caca4",
+    "url": "/mosh-vidly/static/media/fa-brands-400.0fabb660.eot"
+  },
+  {
+    "revision": "cac68c831145804808381a7032fdc7c2",
+    "url": "/mosh-vidly/static/media/fa-brands-400.cac68c83.woff2"
+  },
+  {
+    "revision": "ccfdb9dc442be0c629d331e94497428b",
+    "url": "/mosh-vidly/static/media/fa-brands-400.ccfdb9dc.svg"
+  },
+  {
+    "revision": "dc0bd022735ed218df547742a1b2f172",
+    "url": "/mosh-vidly/static/media/fa-brands-400.dc0bd022.woff"
+  },
+  {
+    "revision": "05b53beb21e3ef13d28244545977152d",
+    "url": "/mosh-vidly/static/media/fa-regular-400.05b53beb.woff"
+  },
+  {
+    "revision": "1a78af4105d4d56e6c34f76dc70bf1bc",
+    "url": "/mosh-vidly/static/media/fa-regular-400.1a78af41.ttf"
+  },
+  {
+    "revision": "3a3398a6ef60fc64eacf45665958342e",
+    "url": "/mosh-vidly/static/media/fa-regular-400.3a3398a6.woff2"
+  },
+  {
+    "revision": "ad3a7c0d77e09602f4ab73db3660ffd8",
+    "url": "/mosh-vidly/static/media/fa-regular-400.ad3a7c0d.eot"
+  },
+  {
+    "revision": "e75dfd904d366a2560c63c23cfc98ef8",
+    "url": "/mosh-vidly/static/media/fa-regular-400.e75dfd90.svg"
+  },
+  {
+    "revision": "03ba7cb710104df27f1c9c46d64bee4e",
+    "url": "/mosh-vidly/static/media/fa-solid-900.03ba7cb7.svg"
+  },
+  {
+    "revision": "781e85bb50c8e8301c30de56b31b1f04",
+    "url": "/mosh-vidly/static/media/fa-solid-900.781e85bb.ttf"
+  },
+  {
+    "revision": "89bd2e38475e441a5cd70f663f921d61",
+    "url": "/mosh-vidly/static/media/fa-solid-900.89bd2e38.eot"
+  },
+  {
+    "revision": "c500da19d776384ba69573ae6fe274e7",
+    "url": "/mosh-vidly/static/media/fa-solid-900.c500da19.woff2"
+  },
+  {
+    "revision": "ee09ad7553b8ad3d81150d609d5341a0",
+    "url": "/mosh-vidly/static/media/fa-solid-900.ee09ad75.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/mosh-vidly/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/mosh-vidly/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/mosh-vidly/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/mosh-vidly/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/mosh-vidly/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
